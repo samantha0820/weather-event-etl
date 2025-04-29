b87afc0c8cbcfaa9009a445c2c25531dd80beb16
samantha0820 <88820lm98@gmail.com>
2025-04-28 20:55:54 -0400

    newnew

--- load.py
+++ load.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-def save_to_csv(weather_df, event_df, path_prefix="weather-event-etl/output"):
+def save_to_csv(weather_df, event_df, path_prefix="output"):
     os.makedirs(path_prefix, exist_ok=True)
     weather_df.to_csv(f"{path_prefix}/weather_forecast.csv", index=False)
     event_df.to_csv(f"{path_prefix}/events_forecast.csv", index=False)
